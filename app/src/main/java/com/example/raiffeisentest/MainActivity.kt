b04efda37c6b227515a88ee450a68c4db12763e6
Ciufudean Dani <daniel.ciufudean@golfgenius.com>
2022-11-10 16:33:39 +0200

    add toolbar and make UI changes

--- app/src/main/java/com/example/raiffeisentest/MainActivity.kt
+++ app/src/main/java/com/example/raiffeisentest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.raiffeisentest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.example.raiffeisentest.databinding.ActivityMainBinding
 
 
@@ -12,5 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+
+        menuInflater.inflate(R.menu.main_manu, menu)
+        return true
     }
 }
