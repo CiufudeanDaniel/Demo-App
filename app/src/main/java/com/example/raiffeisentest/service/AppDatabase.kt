aa2c59d4b558e2372bb7efd83ff4caa930106e3b
Ciufudean Dani <daniel.ciufudean@golfgenius.com>
2022-11-13 17:06:30 +0200

    add security for database

--- app/src/main/java/com/example/raiffeisentest/service/AppDatabase.kt
+++ app/src/main/java/com/example/raiffeisentest/service/AppDatabase.kt
@@ -6,6 +6,9 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.raiffeisentest.interfaces.UserDao
 import com.example.raiffeisentest.models.User
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+
 
 @Database(entities = [User::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
@@ -21,11 +24,16 @@ abstract class AppDatabase : RoomDatabase() {
                 return tempInstance
             }
             synchronized(this) {
+                val userEnteredPassphrase: CharArray = charArrayOf('p','a', 's', 's', 'w','o','r','d')
+                val passphrase: ByteArray = SQLiteDatabase.getBytes(userEnteredPassphrase)
+                val factory = SupportFactory(passphrase)
                 val instance = Room.databaseBuilder(
                     context,
                     AppDatabase::class.java,
                     "app_database"
-                ).build()
+                )
+                    .openHelperFactory(factory)
+                    .build()
                 INSTANCE = instance
                 return instance
             }
